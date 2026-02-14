26c809d02bd910e83ec9b826c20e3ef3950fd185
Isasa2357 <isasa_egwork@outlook.com>
2026-02-14 10:53:26 +0900

    comit

--- VarjoDataStreamServer/VarjoVSTFrame/VarjoVSTFrameDispatcher.cpp
+++ VarjoDataStreamServer/VarjoVSTFrame/VarjoVSTFrameDispatcher.cpp
@@ -4,49 +4,13 @@
 
 namespace VarjoVSTFrame {
 
-	VarjoVSTFramedataDispatcher::VarjoVSTFramedataDispatcher(const VarjoVSTFramedataDispatcherOptions& options) {
-		if (options.writer_option) {
-			if (options.writer_parallel_write_option) {
-				this->writer_ = std::unique_ptr<VarjoVSTParallelVideoWriter>(
-					new VarjoVSTParallelVideoWriter(
-						options.width,
-						options.height,
-						options.row_stride,
-						options.writer_codec,
-						options.writer_out_path,
-						options.writer_crf,
-						options.writer_framerate,
-						options.writer_buffer_size
-					)
-				);
-			} else {
-				this->writer_ = std::unique_ptr<VarjoVSTSerialVideoWriter>(
-					new VarjoVSTSerialVideoWriter(
-						options.width,
-						options.height,
-						options.row_stride,
-						options.writer_codec,
-						options.writer_out_path,
-						options.writer_crf,
-						options.writer_framerate,
-						)
-				);
-			}
+	class Dispatcher : public ISubmitFrame {
+	public:
+		Dispatcher();
 
-			if (options.previewer_option) {
-				if (options.preview_parallel_option) {
-					this->previewer_ = std::unique_ptr<VarjoVSTParallelVideoPreviewer>(
-						new VarjoVSTParallelVideoPreviewer(options.width, options.height, 0)
-					);
-				} else {
-					this->previewer_ = std::unique_ptr<VarjoVSTSerialVideoPreviewer>(
-						new VarjoVSTSerialVideoPreviewer(options.width, options.height, 0)
-					);
-				}
-			}
-		}
-	}
-	void VarjoVSTFramedataDispatcher::dispatch(const VSTCamStreamer::Framedata& framedata) {
 
-	}
+	private:
+		std::vector<std::unique_ptr<ISubmitFramedata>> submit_framedata_sink_;
+		std::vector<std::unique_ptr<ISubmitMetadata>> submit_metadata_sink_;
+	};
 }
