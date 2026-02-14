93687dbc8d9511ff9fbe6e91b3c027f76e8d0d6c
Isasa2357 <iwatake.s.b573@m.isct.ac.jp>
2026-02-14 17:40:57 +0900

    両眼対応のVarjoVSTFrame::VideoWriterを実装

---
+++ VarjoDataStreamServer/VarjoVSTFrame/DataLogger.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "varjo_vst_frame_type.hpp"
+#include "VarjoVSTCamStreamer.hpp"
+#include "VarjoVSTVideoPreviewer.hpp"
+#include "VarjoVSTVideoWriter.hpp"
+#include "ISubmitFrame.hpp"
+
+namespace VarjoVSTFrame {
+
+	/**
+	 * @brief フレームデータをディスクに書き込むクラス．
+	 * @detail 医者からのデータセット採取に間に合わせるにはServerを作る時間がなかったため，実装．本実装では使わない
+	 */
+	class DataLogger {
+		
+	
+	};
+}
+
