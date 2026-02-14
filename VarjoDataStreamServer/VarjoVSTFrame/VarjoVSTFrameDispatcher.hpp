1b3aad98af9c820f4655bce11bfd47673cb2e46a
Isasa2357 <isasa_egwork@outlook.com>
2026-02-14 10:53:26 +0900

    comit

--- VarjoDataStreamServer/VarjoVSTFrame/VarjoVSTFrameDispatcher.hpp
+++ VarjoDataStreamServer/VarjoVSTFrame/VarjoVSTFrameDispatcher.hpp
@@ -1,25 +1,14 @@
 #pragma once
 
+#include <vector>
+
 #include "VarjoVSTCamStreamer.hpp"
 #include "VarjoVSTVideoPreviewer.hpp"
 #include "VarjoVSTVideoWriter.hpp"
 #include "ISubmitFrame.hpp"
+#include "varjo_vst_frame_type.hpp"
 
 namespace VarjoVSTFrame {
 
-	class VarjoVSTFramedataDispatcher: public ISubmitFrame {
-	public:
-		VarjoVSTFramedataDispatcher();
 
-		void open_previewer(const VarjoVSTVideoPreviewerOptions previwer_opts);
-
-		void open_videoWriter(const VarjoVSTVideoWriterOptions videoWriter_opts);
-
-
-	private:
-		std::vector<std::unique_ptr<ISubmitFrame>> submitFrameSinks_;
-		std::vector<std::unique_ptr<ISubmitFramePair>> submitFramePairSinks_;
-		std::vector<std::unique_ptr<ISubmitFramedata>> submitFramedataSinks_;
-		std::vector<std::unique_ptr<ISubmitMetadata>> submitMetadataSinks_;
-	}
 }
